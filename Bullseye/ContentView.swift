54d19422eabf77f32db49deb0fabfbc036e3ca92
Tin Trinh <trinh.tin@hamee.co.jp>
2020-04-24 19:58:28 +0900

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by trinh.tin on 2020/04/24.
+//  Copyright © 2020 New Tetra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
